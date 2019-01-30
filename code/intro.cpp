b0a88fcbc73ac717998cd8f48685b7c53fedfcae
gcallah <gcallah@mac.com>
2019-01-30 18:18:57 -0500

    Added funcs f, f1 and f2.

--- code/intro.cpp
+++ code/intro.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void f(int n)
+{
+    n++;
+}
+
+
+void f1(int& n)
+{
+    n++;
+}
+
+
+void f2(const int& n)
+{
+    n++;
+}
+
+
 int main()
 {
     cout << "Hello world!\n";
+    int n = 7;
+    f(n);
+    cout << n << endl;
+    f1(n);
+    cout << n << endl;
 }
