e4c1679f2f08b41c546db8908ffec2c68778103f
gcallah <gcallah@mac.com>
2019-02-01 00:27:26 -0500

    Added Notebook for Python list examples.

--- code/py_list.py
+++ code/py_list.py
@@ -2,10 +2,12 @@
 A test of how a python iterator passes back items
 """
 
-l = [1, 2, 3, 5, 8, 13, 21]
+l = []
+for i in range(8):
+    l.append(i)
 
 for num in l:
-    num += 1
+    num += 10
     print("num inside first iterator = " + str(num))
 
 for num in l:
