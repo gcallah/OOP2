f1aea05e48fa3e20dff37da98ddc728607e666f0
gcallah <gcallah@mac.com>
2019-02-04 17:30:15 -0500

    Added func declarations.

--- code/funcs.cpp
+++ code/funcs.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+const bool DEBUG = false;
+
+void pass_by_value(int n);
+void pass_by_ref(int &n);
+int call_with_ret(int n);
+void pass_by_constref(const int& n);
+
+
+int main()
+{
+    int n = 7;
+    pass_by_value(n);
+    if(DEBUG)
+        cout << "after pass_by_value, n = " << n << endl;
+    pass_by_ref(n);
+    if(DEBUG)
+        cout << "after pass_by_ref, n = " << n << endl;
+    int n2 = call_with_ret(n);
+    if(DEBUG)
+        cout << "after call_with_ret, n2 = " << n2 << endl;
+    cout << "The square of " << n << " is " << n2 << endl;
+}
+
 void pass_by_value(int n)
 {
     n++;
@@ -26,14 +49,3 @@ int call_with_ret(int n)
 //    n++;
 // }
 
-
-int main()
-{
-    int n = 7;
-    pass_by_value(n);
-    cout << "after pass_by_value, n = " << n << endl;
-    pass_by_ref(n);
-    cout << "after pass_by_ref, n = " << n << endl;
-    int n2 = call_with_ret(n);
-    cout << "after call_with_ret, n2 = " << n2 << endl;
-}
