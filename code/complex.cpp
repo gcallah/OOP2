c83ef1b444853b84e766a4ce1870e4dce3d83909
gcallah <gcallah@mac.com>
2019-02-04 17:50:54 -0500

    Adding formatting for complex numbers.

--- code/complex.cpp
+++ code/complex.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 const bool DEBUG = true;
@@ -10,19 +11,8 @@ struct Complex
 };
 
 
-void printComplex(const Complex& c)
-{
-    cout << "R: " << c.real << " I: " << c.imaginary << endl;
-}
-
-
-Complex add(const Complex& c1, const Complex& c2)
-{
-    Complex result;
-    result.real = c1.real + c2.real;
-    result.imaginary = c1.imaginary + c2.imaginary;
-    return result;
-}
+void printComplex(const Complex& c);
+Complex add(const Complex& c1, const Complex& c2);
 
 
 int main()
@@ -32,6 +22,11 @@ int main()
     cout << "Input a complex number:\n";
     Complex c1;
     cin >> c1.real >> c1.imaginary;
+    if(!cin)
+    {
+        cerr << "Bad input format\n";
+        exit(1);
+    }
     if(DEBUG)
     {
         cout << "c1 = ";
@@ -48,3 +43,21 @@ int main()
     Complex c3 = add(c1, c2);
     printComplex(c3);
 }
+
+
+void printComplex(const Complex& c)
+{
+    cout << setprecision(10) << "R: " << c.real << " I: " 
+         << c.imaginary << endl;
+}
+
+
+Complex add(const Complex& c1, const Complex& c2)
+{
+    Complex result;
+    result.real = c1.real + c2.real;
+    result.imaginary = c1.imaginary + c2.imaginary;
+    return result;
+}
+
+
