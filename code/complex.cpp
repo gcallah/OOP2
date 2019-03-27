10a11ac15c77205d897f8945d49486a17026daa0
gcallah <gcallah@mac.com>
2019-03-27 14:16:37 -0400

    Changed names of complex cpp files.

--- code/complex.cpp
+++ code/complex.cpp
@@ -6,16 +6,48 @@ using namespace std;
 
 const bool DEBUG = true;
 
-struct Complex
+class Complex
 {
-    double real;
-    double imaginary;
+    friend ostream& operator<< (ostream& os, const Complex& c);
+    friend istream& operator>> (istream& is, Complex& c);
+
+    public:
+    
+        Complex(double real=0.0, double imag=0.0) : real{real}, imag{imag}
+        {}
+
+        // we will write:
+        // +=, ==, *=, *, -, -=, /, /=
+        // we will re-write +
+
+        Complex operator+(const Complex& c)
+        {
+            Complex sum{real + c.real, imag + c.imag};
+            return sum;
+        }
+
+    private:
+        double real;
+        double imag;
 };
 
 
+ostream& operator<< (ostream& os, const Complex& c)
+{
+    os << setprecision(10) << c.real << showpos 
+         << c.imag << "i" << noshowpos << endl;
+    return os;
+}
+
+
+istream& operator>> (istream& is, Complex& c)
+{
+    is >> c.real >> c.imag;
+    return is;
+}
+
+
 void printVector(const vector<Complex>& v);
-void printComplex(const Complex& c);
-Complex add(const Complex& c1, const Complex& c2);
 
 
 int main()
@@ -24,7 +56,7 @@ int main()
     // We will work with complex numbers.
     cout << "Input a complex number:\n";
     Complex c1;
-    cin >> c1.real >> c1.imaginary;
+    cin >> c1;
     if(!cin)
     {
         cerr << "Bad input format\n";
@@ -32,19 +64,16 @@ int main()
     }
     if(DEBUG)
     {
-        cout << "c1 = ";
-        printComplex(c1);
+        cout << "c1 = " << c1 << endl;
     }
 
     Complex c2{43.2, 58.9};
     if(DEBUG) 
     {
-        cout << "c2 = ";
-        printComplex(c2);
+        cout << "c2 = " << c2 << endl;
     }
 
-    Complex c3 = add(c1, c2);
-    printComplex(c3);
+    Complex c3 = c1 + c2;
 
     vector<Complex> v{c1, c2, c3};
     cout << "Printing vector\n";
@@ -52,27 +81,12 @@ int main()
 }
 
 
-void printComplex(const Complex& c)
-{
-    cout << setprecision(10) << "R: " << c.real << " I: " 
-         << c.imaginary << endl;
-}
-
 
 void printVector(const vector<Complex>& v)
 {
     for(auto c : v)
     {
-        printComplex(c);
+        cout << c;
     }
-}
-
-
-Complex add(const Complex& c1, const Complex& c2)
-{
-    Complex result;
-    result.real = c1.real + c2.real;
-    result.imaginary = c1.imaginary + c2.imaginary;
-    return result;
 }
 
