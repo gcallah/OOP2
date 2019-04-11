a38e3e67de6b46ddba533a9845f530be8b0a9b2c
gcallah <gcallah@mac.com>
2019-04-10 22:38:38 -0400

    Adding extensive educational comments to source code files.

--- code/complex.cpp
+++ code/complex.cpp
@@ -1,4 +1,8 @@
-// Creating a new type using struct:
+/*
+ * FILE: complex.cpp
+ * THEME: operator over-loading.
+ * CLASSES: Complex
+ * */
 #include <iostream>
 #include <iomanip>
 #include <vector>
@@ -6,47 +10,66 @@ using namespace std;
 
 const bool DEBUG = true;
 
-class Complex
-{
+class Complex {
+    /*
+     * A partial implementation of a complex number class,
+     * designed to illustrate operator over-loading.
+     * */
     friend ostream& operator<< (ostream& os, const Complex& c);
     friend istream& operator>> (istream& is, Complex& c);
 
-    friend bool operator== (const Complex& c1, const Complex& c2)
-    {
+    friend bool operator== (const Complex& c1, const Complex& c2) {
+        /*
+         * The equality operator is a friend since it is binary
+         * and symmetrical.
+         * */
         return ((c1.real == c2.real) && (c1.imag == c2.imag));
     }
 
     public:
     
         Complex(double real=0.0, double imag=0.0)
-            : real{real}, imag{imag}
-        {}
+            : real{real}, imag{imag} {}
 
-        // we will write:
-        // +=, ==, *=, *, -, -=, /, /=
-        // ++ (pre and post), ==
-        // we will re-write +
-
-        explicit operator bool() const
-        {
+        explicit operator bool() const {
+            /*
+             * The `bool` operator tests to see if either
+             * data member is non-zero, and returns `true`
+             * if so.
+             * */
             return ((real != 0) || (imag != 0));
         }
 
-        Complex& operator++()  // pre-increment
-        {
+        Complex& operator++() {
+            /*
+             * This overload is pre-increment: pretty straightforward:
+             * bump up the `real` member, and return myself.
+             * Of course, it's not obvious that this is what incrementing
+             * a complex number *should* do: but we are teaching here,
+             * not writing a production Complex class.
+             * */
             ++real;
             return (*this);
         }
 
-        Complex operator++(int dummy)  // post-increment
-        {
+        Complex operator++(int dummy) {
+            /*
+             * Post-increment is more complicated than pre: first
+             * of all, it takes a dummy int parameter (unused)
+             * just so the compiler can tell which inc operator is which.
+             * Secondly, since it returns the value from *before*
+             * the increment, we must store that in a temp var.
+             * */
             Complex temp(*this);
             real++;
             return temp;
         }
 
-        Complex operator+(const Complex& c)
-        {
+        Complex operator+(const Complex& c) {
+            /*
+             * `+` is very straightforward, and should be the first operator
+             * taught.
+             * */
             Complex sum{real + c.real, imag + c.imag};
             return sum;
         }
@@ -57,16 +80,23 @@ class Complex
 };
 
 
-ostream& operator<< (ostream& os, const Complex& c)
-{
+ostream& operator<< (ostream& os, const Complex& c) {
+    /*
+     * Outputting a `Complex` instance, while illustrating some of the
+     * capabilities of I/O streams: `setprecision` gives us a fixed
+     * number of decimal places, while `showpos` turns on the plus
+     * sign for positive numbers.
+     * */
     os << setprecision(10) << c.real << showpos 
          << c.imag << "i" << noshowpos << endl;
     return os;
 }
 
 
-istream& operator>> (istream& is, Complex& c)
-{
+istream& operator>> (istream& is, Complex& c) {
+    /*
+     * Read a `Complex` number from an input stream.
+     * */
     is >> c.real >> c.imag;
     return is;
 }
@@ -77,8 +107,6 @@ void printVector(const vector<Complex>& v);
 
 int main()
 {
-    // A program to introduce `struct`.
-    // We will work with complex numbers.
     cout << "Input a complex number:\n";
     Complex c1;
     cin >> c1;
