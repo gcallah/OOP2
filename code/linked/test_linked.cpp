9708bf406fc993ccaf98e4475f38700bb5784547
gcallah <gcallah@mac.com>
2019-05-11 04:00:53 -0400

    Adding asserts in tests and topics in index.

--- code/linked/test_linked.cpp
+++ code/linked/test_linked.cpp
@@ -3,6 +3,7 @@
  * A test "suite" for our linked list code.
  * */
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 #include "linked.h"
@@ -20,24 +21,33 @@ int main() {
     add_at_end(head, 128);
     add_at_end(head, 256);
     add_at_front(head, 8);
+    assert(head->data == 8);
+
     // let's see if our last() function works:
     Node* lastp = last(head);
+    assert(lastp->data == 256);
     cout << "Last: " << lastp << endl;
+
     cout << "List: \n";
     print_list(cout, head);
     Node* rev = reverse(head);
+    assert(rev->data == 256);
     cout << "reverse: \n";
     print_list(cout, rev);
+
     Node* head2 = duplicate(head);
     del_head(head);
     // &head gives us a pointer to a pointer
     del_tail(&head);
     del_tail(&head);
+    assert(head->data == 16);
     cout << "List after deletes: \n";
     print_list(cout, head);
+
     Node* l2 = nullptr;
     cout << "Last l2: " << last(l2) << endl;
     bool worked = del_head(l2);
+    assert(!worked);
     cout << "Did del_head() work? " << worked << endl;
     print_list(cout, head2);
 }
