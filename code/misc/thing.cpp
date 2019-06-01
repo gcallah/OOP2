6159011de932ee2ccd0397682d06ab0a62b5d609
gcallah <gcallah@mac.com>
2019-06-01 17:38:17 -0400

    Added more programs as html pages.

--- code/misc/thing.cpp
+++ code/misc/thing.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 using namespace std;
 
+/*
+ * A simple structure with a constructor.
+ * */
 struct Thing {
     int val;
     Thing(int i) : val(i) {}
 };
 
+/*
+ * Allocate an array of 100 Thing pointers,
+ * fill it with Things, change their values,
+ * and then delete them:
+ * */
 int main() {
+    // allocate array:
     Thing** datap = new Thing*[100];
+
+    // Fill array with Things:
     for (int i = 0; i < 100; i++)
         datap[i] = new Thing(i);
 
+    // Change their values:
     for (int i = 0; i < 100; i++) {
         datap[i]->val += i;
-        cout << datap[i]->val << " ";
+        cout << "datap " << i << " = " << datap[i]->val << " ";
     }
     cout << endl;
+
+    // delete them:
+    for (int i = 0; i < 100; i++) {
+        delete datap[i];
+    }
 }
