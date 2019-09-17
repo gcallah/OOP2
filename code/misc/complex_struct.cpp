2eedebf07ea52049f265eae7ce3482df8cd77e26
gcallah <gcallah@mac.com>
2019-09-17 12:19:06 -0400

    Added docstring for complex_struct.cpp.

--- code/misc/complex_struct.cpp
+++ code/misc/complex_struct.cpp
@@ -1,6 +1,6 @@
 /*
- * Creating a new type using class.
- * This is just an intermediate in-class version.
+ *  This file is for taking a struct into a class
+ *  for an inclass demo.
  * */
 #include <iostream>
 #include <iomanip>
@@ -9,8 +9,25 @@ using namespace std;
 
 const bool DEBUG = true;
 
-struct Complex
+class Complex
 {
+ public:
+    Complex(double r=0.0, double im=0.0) : real(r), imaginary(im) { }
+
+    Complex add(const Complex& other)
+    {
+        Complex result;
+        result.real = real + other.real;
+        result.imaginary = imaginary + other.imaginary;
+        return result;
+    }
+    double get_real() const { return real; }
+    double get_imaginary() const { return imaginary; }
+
+    void set_real(double r) { real = r; }
+    void set_imaginary(double r) { imaginary = r; }
+
+ private:
     double real;
     double imaginary;
 };
@@ -18,27 +35,30 @@ struct Complex
 
 void printVector(const vector<Complex>& v);
 void printComplex(const Complex& c);
-Complex add(const Complex& c1, const Complex& c2);
 
 
 int main()
 {
     // A program to introduce `struct`.
     // We will work with complex numbers.
-    cout << "Input a complex number:\n";
     Complex c1;
-    cin >> c1.real >> c1.imaginary;
+    printComplex(c1);
+    cout << "\nInput a complex number:\n";
+    double r, i;
+    cin >> r >> i;
     if(!cin)
     {
         cerr << "Bad input format\n";
         exit(1);
     }
+    c1.set_real(r); c1.set_imaginary(i);
     if(DEBUG)
     {
         cout << "c1 = ";
         printComplex(c1);
     }
 
+    /*
     Complex c2{43.2, 58.9};
     if(DEBUG) 
     {
@@ -46,36 +66,28 @@ int main()
         printComplex(c2);
     }
 
-    Complex c3 = add(c1, c2);
+    Complex c3 = c1.add(c2);
     printComplex(c3);
 
     vector<Complex> v{c1, c2, c3};
     cout << "Printing vector\n";
     printVector(v);
+    */
 }
 
 
 void printComplex(const Complex& c)
 {
-    cout << setprecision(10) << "R: " << c.real << " I: " 
-         << c.imaginary << endl;
+    cout << setprecision(10) << "R: " << c.get_real() << " I: " 
+         << c.get_imaginary() << endl;
 }
 
 
 void printVector(const vector<Complex>& v)
 {
-    for(const Complex& c : v)
+    for(auto c : v)
     {
         printComplex(c);
     }
-}
-
-
-Complex add(const Complex& c1, const Complex& c2)
-{
-    Complex result;
-    result.real = c1.real + c2.real;
-    result.imaginary = c1.imaginary + c2.imaginary;
-    return result;
 }
 
