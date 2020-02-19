d51a805a9388fbf24087dd017a5589ac46cce413
gcallah <gcallah@mac.com>
2020-02-19 01:26:27 -0500

    More explanations in mem.cpp.

--- code/misc/finished_code/mem.cpp
+++ code/misc/finished_code/mem.cpp
@@ -10,17 +10,21 @@ using namespace std;
 
 
 int main() {
-    // let's look at the size of some data types:
+    /*
+     * Let's look at the size of some data types:
+     * */
     bool b = true;
     cout << "Size of a bool = " << sizeof(b) << endl;
     char c = 'a';
     cout << "Size of a char = " << sizeof(c) << endl;
-    int i = 0;
+    int i = 28;
     cout << "Size of an integer = " << sizeof(i) << endl;
-    double d = 0.0;
+    double d = 2.71828;
     cout << "Size of a double = " << sizeof(d) << endl;
 
-    // now let's deal with addresses and pointers:
+    /*
+     * Now let's deal with addresses and pointers:
+     * */
     cout << "Address of b = " << &b << endl;
     bool* bptr = &b;
     cout << "Value of bptr = " << bptr << endl;
@@ -37,5 +41,32 @@ int main() {
     cout << "Address of d= " << &d << endl;
     double* dptr = &d;
     cout << "Value of dptr = " << dptr << endl;
+
+    /*
+     * Hmm, what is the size of a pointer?
+     * Is it different for different pointers?
+     * */
+    cout << "Size of a bool ptr = " << sizeof(bptr) << endl;
+    cout << "Size of an integer ptr = " << sizeof(iptr) << endl;
+    cout << "Size of a double ptr = " << sizeof(dptr) << endl;
+
+    /*
+     * Seems like all pointers are the same size! Since C++
+     * references are "really" a sort of pointer, this explains
+     * why, for large objects, it is much more efficient, for 
+     * objects we don't want changed in a function,
+     * (if we want to change the object in a function 
+     * it's clear we *can't* pass a copy!) to pass a
+     * constant reference than to pass a copy: the reference
+     * will be essentially an 8-byte address, while the copy 
+     * might be a multi-gigabyte video segment.
+     *
+     * So, if we have pointer to some value, how do we get
+     * the value? We *dereference* the pointer.
+     * */
+    cout << "Value pointed to by bptr = " << *bptr << endl;
+    cout << "Value pointed to by iptr = " << *iptr << endl;
+    cout << "Value pointed to by dptr = " << *dptr << endl;
+
 }
 
