002d684fe4c67ad5260a2d241ff5cc0d8c015aac
gcallah <gcallah@mac.com>
2020-03-09 13:36:45 -0400

    Added starting_code/cyclical.cpp

---
+++ code/misc/starting_code/cyclical.cpp
@@ -0,0 +1,46 @@
+/*
+    testPrincess.cpp
+    Cyclic Association
+    Sec Base
+    Tests the princess and frog_prince classes.
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Princess {
+    public:
+        Princess(string nm) : name(nm) {}
+
+    private:
+        string name;
+};
+
+
+class Frog {
+    public:
+        Frog(string nm) : name(nm) {}
+
+    private:
+        string name;
+};
+
+
+int main() {
+    Princess snowy("Snow White");
+//    cout << snowy << endl;
+    Frog froggy("Froggy");
+//    cout << froggy << endl;
+//    snowy.marries(&froggy);
+//    cout << snowy << endl;
+//
+//    Princess sleeping("Sleeping Beauty");
+//    Frog bullfrog("Bull Frog");
+//    bullfrog.marries(&sleeping);
+//    cout << sleeping << endl;
+//    cout << bullfrog << endl;
+    exit(0);
+}
+
+
