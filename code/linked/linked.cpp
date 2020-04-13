fae587b10ef5cc50ca434eb9c9245fc04465197d
gcallah <gcallah@mac.com>
2020-04-13 16:37:21 -0400

    Lecture 1 of linked lists done.

--- code/linked/linked.cpp
+++ code/linked/linked.cpp
@@ -25,17 +25,6 @@ ostream& operator<<(ostream& os, Node* nd) {
 }
 
 
-void add_at_front(Node*& head, int data) {
-}
-
-
-void print_list(ostream& os, Node* head, string title="") {
-    /*
-     * Given the head of a list, print the whole thing.
-     * */
-}
-
-
 Node* rfind(Node* head, int item) {
     return head;
 }
@@ -56,12 +45,54 @@ Node* clone(Node* orig_head) {
 }
 
 
+Node* last(Node* head) {
+    if (!head) return nullptr;
+    else {
+        Node* np = head;
+        while (np->next) np = np->next;
+        return np;
+    }
+}
+
+
+void add_at_front(Node*& head, int data) {
+    head = new Node(data, head);
+}
+
+
+void add_at_end(Node* head, int data) {
+    Node* end = last(head);
+    if (!end)
+        add_at_front(head, data);
+    else
+        end->next = new Node(data);
+}
+
+
+void print_list(ostream& os, Node* head, string title="") {
+    /*
+     * Given the head of a list, print the whole thing.
+     * */
+    if (title.size()) os << title << ": ";
+    if (head) {
+        os << head << " --> ";
+        print_list(os, head->next);
+    }
+    else os << "NULL" << endl;
+}
+
+
 int main() {
     Node* list1 = nullptr;
-//    add_at_front(list1, 2);
-//    add_at_front(list1, 4);
-//    add_at_front(list1, 8);
-//    add_at_front(list1, 16);
+    add_at_front(list1, 2);
+    add_at_front(list1, 4);
+    add_at_front(list1, 8);
+    add_at_front(list1, 16);
+    Node* last_node = last(list1);
+    cout << "last node of list1 = " << last_node << endl;
+    add_at_end(list1, 1);
+    last_node = last(list1);
+    cout << "last node of list1 = " << last_node << endl;
 //    Node* new_list = rclone(list1);
 //    Node* four = rfind(list1, 4);
 //    insert(four, 5);
